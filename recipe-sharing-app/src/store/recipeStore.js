b8739ec2686c4e914ffd2b87b5c775e99e0ad0f2
Irene <irenekilosh2@gmail.com>
2025-07-24 12:12:47 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -3,19 +3,16 @@ import { create } from 'zustand';
 export const useRecipeStore = create((set) => ({
   recipes: [],
 
-  // Add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  // Delete a recipe by its ID
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 
-  // Update an existing recipe
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
@@ -23,7 +20,7 @@ export const useRecipeStore = create((set) => ({
       ),
     })),
 
-  // Replace the entire recipe list
   setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
+
 
