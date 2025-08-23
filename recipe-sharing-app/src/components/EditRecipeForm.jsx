7d390e9c7278e2b11d3b1ea8096c79d8da3f3ff7
Irene <irenekilosh2@gmail.com>
2025-08-23 22:19:29 +0300

    Update EditRecipeForm.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -6,22 +6,17 @@ const EditRecipeForm = ({ recipe }) => {
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-<<<<<<< HEAD
   const handleSubmit = (e) => {
     e.preventDefault();
-=======
   const handleSubmit = (event) => {
     event.preventDefault(); // 
->>>>>>> 88a0050a03e5e51c49d765e9039a0d6cad74632e
     updateRecipe({ ...recipe, title, description });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-<<<<<<< HEAD
       <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-=======
       <input
         value={title}
         onChange={(e) => setTitle(e.target.value)}
@@ -32,7 +27,6 @@ const EditRecipeForm = ({ recipe }) => {
         onChange={(e) => setDescription(e.target.value)}
         placeholder="Description"
       />
->>>>>>> 88a0050a03e5e51c49d765e9039a0d6cad74632e
       <button type="submit">Save Changes</button>
     </form>
   );
